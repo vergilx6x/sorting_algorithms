111703b5a395e20d30679004b9010d83e1ca7dd4
vergilx6x <med.work96@hotmail.com>
2024-04-30 13:31:06 -0700

    fix

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -3,19 +3,25 @@
 #include "sort.h"
 
 
-void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
+/**
+ * swap_nodes - Sorts a doubly linked list.
+ * @list: A pointer to the head of the list.
+ * @node_1: First node to swap
+ * @node_2: second node to swap
+ */
+void swap_nodes(listint_t **list, listint_t **node_1, listint_t *node_2)
 {
-	(*n1)->next = n2->next;
-	if (n2->next != NULL)
-		n2->next->prev = *n1;
-	n2->prev = (*n1)->prev;
-	n2->next = *n1;
-	if ((*n1)->prev != NULL)
-		(*n1)->prev->next = n2;
+	(*node_1)->next = node_2->next;
+	if (node_2->next != NULL)
+		node_2->next->prev = *node_2;
+	node_2->prev = (*node_1)->prev;
+	node_2->next = *node_1;
+	if ((*node_1)->prev != NULL)
+		(*node_1)->prev->next = node_2;
 	else
-		*h = n2;
-	(*n1)->prev = n2;
-	*n1 = n2->prev;
+		*list = node_2;
+	(*node_1)->prev = node_2;
+	*node_1 = node_2->prev;
 }
 
 /**
